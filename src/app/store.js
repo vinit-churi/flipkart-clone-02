17c7b5df1b3fefa232ac0da72bd0651b9b81ddf4
VINIT CHURI <vinitchuri@hotmail.com>
2023-06-02 15:52:27 +0530

    Delete src directory

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartSlice from "../feature/cartSlice";
-
-export const store = configureStore({
-  reducer: {
-    allCart: cartSlice,
-  },
-});
